eb0db67935b0993143c2f05461aa7c9629f25e23
tanvir10029 <tanvir10029@gmail.com>
2022-03-25 23:09:52 -0400

    completed and added gif

--- app/src/main/java/com/example/parstagram/ParstagramApplication.kt
+++ app/src/main/java/com/example/parstagram/ParstagramApplication.kt
@@ -2,10 +2,14 @@ package com.example.parstagram
 
 import android.app.Application
 import com.parse.Parse
+import com.parse.ParseObject
 
 class ParstagramApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        ParseObject.registerSubclass(Post::class.java)
+
         Parse.initialize(
             Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
